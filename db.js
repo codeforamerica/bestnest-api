3dd58393b53e4cd29d7dc8eb97dcfb045dea2c28
jden <jason@denizac.org>
2014-04-16 20:34:54 -0400

    mock out db and add search and building detail routes

---
+++ db.js
@@ -0,0 +1,37 @@
+// mock db
+var db = {
+  buildings: {
+    byId: function (id) {
+      return Promise.cast({
+        id: id,
+        address: '23 sdfsfs strasse',
+        type: 'Single Family Home',
+        rating: 'ok',
+        energy: {
+          cost: 'hella dollars'
+        },
+        violations: {
+          count: 4,
+          items: [
+            {label: 'Window Screens Not Installed', date: 2323434434232},
+            {label: 'Litter in Yard', date: 2323434434232},
+            {label: 'Inadequate insulation', date: 2323434434232},
+            {label: 'Broken Heater', date: 2323434434232},
+          ]  
+        },
+        owner: {
+          name: 'yeah right',
+          address: 'good luck',
+          email: 'ha!'
+        }
+      })
+    },
+    where: function (predicate) {
+      return Promise.cast([
+        {id: '3', address: '23 dsfds strasse'}
+      ])
+    }
+  }
+}
+
+module.exports = db
--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 var mach = require('mach')
+var querystring = require('querystring')
+require('polyfill-promise')
+var db = require('./db')
 
 var app = mach.stack()
 
 app.get('/', function (req) {
   return "hey sup"
+})
+
+function startsWith(q) {
+  // unsafe, should escape regex chars + check for slow regexps
+  return new RegExp('^' + q)
+}
+
+app.get('/search', function (req) {
+  var params = querystring.parse(req.queryString)
+  console.log(params)
+
+  if (params.q) {
+    // search by address
+    return db.buildings.where({address: startsWith(params.q)})
+      .then(function (docs) {
+        return JSON.stringify({
+          results: docs
+        })
+      })
+  }
+
+  throw new Error('invalid search')
+})
+
+app.get('/buildings/:id', function (req, id) {
+
+  return db.buildings.byId(id)
+    .then(function (doc) {
+      return JSON.stringify(doc)
+    })
+
 })
 
 console.log(app._app._routes)
